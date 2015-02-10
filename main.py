0bece3eaea649564bbca2c3dfa504cf041517acd
Yamist <8bityamist@gmail.com>
2015-02-09 22:35:08 -0800

    Fixed initial os statement, fixed cycle.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import time
 import os
 from itertools import cycle
 
-os.system("xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s /home/yamist/Downloads/test.jpg")
-for i in cycle(range(1, 522)):
+for i in cycle(range(1, 521)):
     time.sleep(2)
     os.system("xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s /home/yamist/Downloads/xfce_background_changer/xfce-animated-wallpaper/img/" + str(i) + ".png")
