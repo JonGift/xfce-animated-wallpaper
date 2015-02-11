c5d91f8180779fbe077d7204eba644b028c16b15
Yamist <8bityamist@gmail.com>
2015-02-11 11:09:43 -0800

    Added in direction option.

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import time
 import os
 from itertools import cycle
 
-for i in cycle(range(1, 521)):
-    time.sleep(2)
-    os.system("xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s /home/yamist/Downloads/xfce_background_changer/xfce-animated-wallpaper/img/" + str(i) + ".png")
+def wallpaper(i):
+    #Change image path here.
+    os.system("xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s /home/yamist/Downloads/xfce-animated-wallpaper/img/" + str(i) + ".png")
+
+direction = input('Input (left/right)')
+
+if direction.lower() == 'left':
+    for i in cycle(reversed(range(1, 521))):
+        time.sleep(2)
+        wallpaper(i)
+elif direction.lower() == 'right':
+    for i in cycle(range(1, 521)):
+        time.sleep(2)
+        wallpaper(i)
